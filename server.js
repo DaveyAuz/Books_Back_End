259202f2dac532652964507d1c3f8bf6764d9889
DaveyAuz <doswald27@gmail.com>
2023-03-29 06:14:14 -0400

    added mongoose and mongo DB URL

--- server.js
+++ server.js
@@ -4,15 +4,51 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+// **** REQUIRE IN OUR MONGOOSE LIBRARY **** //
+const mongoose = require('mongoose');
+
 const app = express();
+
+// Middleware
 app.use(cors());
 
+// define PORT validate env is working
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+// LISTEN
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
 
-  response.send('test request received')
+// **** DATABASE CONNECTION MONGOOSE ********** //
+// ** PER THE MONGOOSE LIBRARY PLUG AND PLAY ** //
 
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
 })
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+// Routes
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome To The Server');
+});
+
+app.get('*', (request, response) => {
+  response.status(404).send('Page Not Found');
+});
+
+// ERROR HANDLER
+app.use((error, request, response, next) => {
+console.log(error.message);
+  response.status(500).send(error.message);
+})
+
+
+
+
+app.get('/test', (request, response) => {
+  response.send('test request received')
+})
+
+app.listen(PORT, () => console.log(`listening on Port ${PORT}`));
